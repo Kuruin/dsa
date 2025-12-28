f43611d7a3a8f996edd904c3315f41bb01d1e65f
Kuruin <manavjit9@gmail.com>
2025-12-28 21:03:47 +0530

    Primitive Types

---
+++ Python_Basics/2-Primitive Types/main.py
@@ -0,0 +1,38 @@
+print("Hello")  # inbuilt function
+
+a = 1
+b = 'this is a string'
+c = """
+This is multiline strings
+"""
+print(b[0])
+print(b[0:3])  # slicing
+# here \ is a escape char
+print("Heloo \"you!")
+# \\
+# \'
+# \n
+
+# formatted strings
+print(f'{b}')
+print(len(b))  # it counts the spaces too
+print(f'This is the last character: {b[-1]}')
+print(b.upper())
+print("this" in b, "wow" in b)
+# b. this will display all the methods available for the string datatype
+
+
+#
+# / - divisoion (float)
+# // - div (int)
+# % - rem
+# ** - ( augmented operator)
+print(10 ** 3)
+# import math
+# round, abs - absoulte value , ceil - upper value , floor - down value
+#
+
+
+# type conversion
+# int(), float(), str(), bool()
+# fal   sy,"", 0, none - these are bool false values
