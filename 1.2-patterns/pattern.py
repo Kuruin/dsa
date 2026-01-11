cd6eaefa519b684c44c980ec8acd747d37ba612d
Kuruin <manavjit9@gmail.com>
2026-01-12 00:14:46 +0530

    Pattern 1-5

--- 1.2-patterns/pattern.py
+++ 1.2-patterns/pattern.py
@@ -1,10 +1,37 @@
 class Solution:
-    def pattern1(self):
-        for i in range(4):
-            for j in range(4):
+    def pattern1(self, n):
+        for i in range(n):
+            for j in range(n):
                 print("*", end=" ")
             print()
 
+    def pattern2(self, n):
+        for i in range(n):
+            print("* " * (i+1))
+
+    def pattern3(self, n):
+        for i in range(n):
+            for j in range(i+1):
+                print(j+1, end=" ")
+            print()
+
+    def pattern4(self, n):
+        for i in range(n):
+            for j in range(i+1):
+                print(i+1, end=" ")
+            print()
+
+    def pattern5(self, n):
+        for i in range(n, 0, -1):
+            print("* "*i)
+
+        # or
+
+        # for i in range(n):
+        #     for j in range(n, i, -1):
+        #         print("*", end=" ")
+        #     print()
+
 
 sol = Solution()
-sol.pattern1()
+sol.pattern5(3)
