26a618ee7041b040f5483233349295546aa1708f
Kuruin <manavjit9@gmail.com>
2026-01-11 22:46:27 +0530

    Pattern 1

---
+++ 1.2-patterns/pattern.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pattern1(self):
+        for i in range(4):
+            for j in range(4):
+                print("*", end=" ")
+            print()
+
+
+sol = Solution()
+sol.pattern1()
