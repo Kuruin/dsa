4e6c13fd2abf0cd3b719d8f728c7fcb2f885fecb
Kuruin <manavjit9@gmail.com>
2026-02-02 23:30:30 +0530

    More recursion

--- 1.4-Recursion/main.py
+++ 1.4-Recursion/main.py
@@ -1,4 +1,6 @@
 from math import factorial
+from array import array
+from re import L
 
 
 class Recursion:
@@ -55,7 +57,36 @@ class Recursion:
             return 1
         return n * self.factorial2(n-1)
 
+    def reverse_array(self, i, arr):  # YOU CAN USE SLICING TOOO [::-1]
+        n = len(arr)
+        if i >= n//2:
+            print(arr)
+            return
+        temp = arr[i]
+        arr[i] = arr[n-i-1]
+        arr[n-i-1] = temp
+        self.reverse_array(i+1, arr)
+
+    def reverse_array_approach2(self, i, r, arr):
+        if i >= r:
+            print(arr)
+            return
+        arr[i], arr[r] = arr[r], arr[i]
+        self.reverse_array_approach2(i+1, r-1, arr)
+
+    def palindrome_string(self, i, string_in):
+        n = len(string_in)
+        string_in = string_in.lower()
+        if i >= n // 2:
+            print("palindrome")
+            return
+        if string_in[i] != string_in[n-i-1]:
+            print("Not a palindrome")
+            return
+        self.palindrome_string(i+1, string_in)
+
 
 if __name__ == "__main__":
     sol = Recursion()
-    sol.factorial2(5)
+    test = array("i", [1, 2, 3, 4, 5])
+    sol.palindrome_string(0, "MaDam")
